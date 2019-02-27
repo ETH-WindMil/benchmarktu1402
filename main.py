d2dfb53bb76fd585322d43799ef73343f2fce406
konnnos <konnos.tatsis@gmail.com>
2019-02-27 18:29:18 +0100

    Added all interface features to the model

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import quadrature
-# import membrane
 import quadrilaterals
 import analysis
 import model
@@ -19,14 +18,33 @@ def main(job):
 
     # settings = job.getSettings()
 
-    # material = settings['temperature']
-    # boundaries = settigns['boundaries']
+    # material = settings['material']
+    # boundaries = settings['boundaries']
     # corrosion = settings['corrosion']
     # temperature = settings['temperature']
 
+    jmaterial = np.array([
+            [1.8e11, 0.3, 10],
+            [1.8e11, 0.3, 25]])
+
+    jthickness = 0.1
+    jwastage = np.array([
+            [0.1, 0.0],
+            [0.1, 0.5]])
+
+    jtemperature = np.array([
+            [10, 0.0],
+            [15, 0.5]])
+
     # }
 
-    ###   Define Geometry
+
+    # { Define damaged areas
+
+    # }
+
+
+    #  Define Geometry
 
     length = 20                 # Dimension in x-axis
 
@@ -73,89 +91,124 @@ def main(job):
     
 
     # {Define elements
-            
+
     elements = []
 
-    elm = quadrilaterals.Quad4()
+    etype = quadrilaterals.Quad4()
     irule = quadrature.Gauss.inQuadrilateral(rule=3).info
 
-    for index in indices:
-        nodesEl = [nodes[index],
-                   nodes[index+nel_y+1],
-                   nodes[index+nel_y+2],
-                   nodes[index+1]]
-        xc = (nodes[index].coords[0]+
-              nodes[index+nel_y+1].coords[0]+
-              nodes[index+nel_y+2].coords[0]+
-              nodes[index+1].coords[0])/4
-    #    th = np.interp(xc, points, widths)
-    #    th = np.interp(xc, [0, 27], [width_start, width_end])
+    for j in indices:
 
-        thickness = np.ones(9)*0.1
-        materials = [mat]*9
+        #  Define element nodes
 
-        # { Interpolate temperature at gauss points
+        enodes = [nodes[j], nodes[j+nel_y+1], nodes[j+nel_y+2], nodes[j+1]]
 
-        # }
+        # Coordinates of element center-point
+
+        xc = np.sum([node.coords[0] for node in enodes])/len(enodes)
+        yc = np.sum([node.coords[1] for node in enodes])/len(enodes)
+
+        # Coordinates of element integration points
+
+        xi = xc+irule[:, 0]*el_size_x
+        yi = yc+irule[:, 1]*el_size_y
+
+        #  Interpolate temperature at gauss points
+
+        temperature = np.interp(xi, jtemperature[:, 1]*length, jtemperature[:, 0])
+
+        #  Define material properties for each integration point
+
+        materials = []
+        E = np.interp(xi, jmaterial[:, 2], jmaterial[:, 0])
+        n = np.interp(xi, jmaterial[:, 2], jmaterial[:, 1])
+
+        for k in range(len(xi)):
+            materials.append(material.LinearElastic(E[k], n[k], density))
+
+        #  Interpolate thickness at gauss points
+
+        wastage = np.interp(xi, jwastage[:, 1]*length, jwastage[:, 0])
+        thickness = jthickness*(np.ones(len(xi))-wastage)
+        thickness = np.ones(9)*0.5
+
+        #  Define elements and modify damaged ones
+
+        elements.append(model.Element(enodes, etype, materials, thickness, irule))
 
 
-        # { Interpolate thickness at gauss points
+    #  Initialize model
 
-        # }
+    model1 = model.Model(nodes, elements)
 
+    #  Apply boundary conditions
 
-        # { Define elements and modify damaged ones
-
-        elements.append(model.Element(nodesEl, elm, materials, thickness, irule))
-
-        # }
-
-    # }
-
-    mesh = model.Model(nodes, elements)
+    kx1, ky1 = 1e15, 1e11
+    kx2, ky2 = 1e15, 1e11
+    kx3, ky3 = 1e15, 1e11
 
     for node in nodes:
         x, y = node.coords[0], node.coords[1]
-        # Left-hand side constraints
+
+        #  Left-hand side constraints
+
         if x == 0 and y == -height_start/2:
-            model.Constraint(mesh).addSpring(node.label, ['x', 'y'], [1e9, 1e9])
+            model1.constraints.addSpring(node.label, ['x', 'y'], [kx1, ky1])
         elif x == el_size_x and y == -height_start/2:
-            model.Constraint(mesh).addSpring(node.label, ['x', 'y'], [1e9, 1e9])
+            model1.constraints.addSpring(node.label, ['x', 'y'], [kx1, ky1])
         elif x == el_size_x*2 and y == -height_start/2:
-            model.Constraint(mesh).addSpring(node.label, ['x', 'y'], [1e9, 1e9])
-        # Mid-point constraints
+            model1.constraints.addSpring(node.label, ['x', 'y'], [kx1, ky1])
+        elif x == el_size_x*3 and y == -height_start/2:
+            model1.constraints.addSpring(node.label, ['x', 'y'], [kx1, ky1])
+        
+        #  Mid-point constraints
+
         elif x == length/2-el_size_x*2 and y == -height_start/2:
-            model.Constraint(mesh).addSpring(node.label, ['x', 'y'], [1e9, 1e9])
+            model1.constraints.addSpring(node.label, ['x', 'y'], [kx1, ky1])
         elif x == length/2-el_size_x and y == -height_start/2:
-            model.Constraint(mesh).addSpring(node.label, ['x', 'y'], [1e9, 1e9])
+            model1.constraints.addSpring(node.label, ['x', 'y'], [kx1, ky1])
         elif x == length/2 and y == -height_start/2:
-            model.Constraint(mesh).addSpring(node.label, ['x', 'y'], [1e9, 1e9])
+            model1.constraints.addSpring(node.label, ['x', 'y'], [kx1, ky1])
         elif x == length/2+el_size_x and y == -height_start/2:
-            model.Constraint(mesh).addSpring(node.label, ['x', 'y'], [1e9, 1e9])
+            model1.constraints.addSpring(node.label, ['x', 'y'], [kx1, ky1])
         elif x == length/2+el_size_x*2 and y == -height_start/2:
-            model.Constraint(mesh).addSpring(node.label, ['x', 'y'], [1e9, 1e9])
-        # Right-hand side constraints
+            model1.constraints.addSpring(node.label, ['x', 'y'], [kx1, ky1])
+        
+        #  Right-hand side constraints
+
+        elif x == length-el_size_x*3 and y == -height_start/2:
+            model1.constraints.addSpring(node.label, ['x', 'y'], [kx1, ky1])
         elif x == length-el_size_x*2 and y == -height_start/2:
-            model.Constraint(mesh).addSpring(node.label, ['x', 'y'], [1e9, 1e9])
+            model1.constraints.addSpring(node.label, ['x', 'y'], [kx1, ky1])
         elif x == length-el_size_x and y == -height_start/2:
-            model.Constraint(mesh).addSpring(node.label, ['x', 'y'], [1e9, 1e9])
+            model1.constraints.addSpring(node.label, ['x', 'y'], [kx1, ky1])
         elif x == length and y == -height_start/2:
-            model.Constraint(mesh).addSpring(node.label, ['x', 'y'], [1e9, 1e9])
+            model1.constraints.addSpring(node.label, ['x', 'y'], [kx1, ky1])
+
+    # }
 
 
-    modal = analysis.Modal(mesh)
-    modal.setNumberOfEigenvalues(10)
-    modal.submit()
+    #  Run analysis
+
+    if janalysis == 'Modal':
+
+        modal = analysis.Modal(model1)
+        modal.setNumberOfEigenvalues(10)
+        modal.submit()
+
+    else:
+
+        pass
 
     print(modal.frequencies)
 
 
-    # { Plot mode shapes
+    #  Plot mode shapes
 
     plt.figure(1)
 
     for mode_no in range(4):
-        for item, mode in zip(list(mesh.ndof2.keys()), modal.modes[:, mode_no]):
+        for item, mode in zip(list(model1.ndof2.keys()), modal.modes[:, mode_no]):
             nodes[item[0]].dsp[item[1]] = mode
 
         n = str(mode_no+1)
@@ -174,12 +227,6 @@ def main(job):
     plt.tight_layout()
     plt.show()
 
-    # }
-
-
-    # { Run analysis
-
-    # }
 
 
     # { Export results
